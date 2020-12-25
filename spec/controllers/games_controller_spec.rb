34e9d035d58cc88c1e595fd764dfca0104ab074c
edwardkv <edwardkv@gmail.com>
2020-12-25 10:29:20 +0400

    Test fifty fifty help

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -96,13 +96,12 @@ RSpec.describe GamesController, type: :controller do
       expect(game_w_questions.current_game_question.help_hash[:fifty_fifty]).not_to be
       expect(game_w_questions.fifty_fifty_used).to be_falsey
 
-      # запрос в контроллен с нужным типом
       put :help, id: game_w_questions.id, help_type: :fifty_fifty
       game = assigns(:game)
       expect(game.finished?).to be_falsey
       expect(game.fifty_fifty_used).to be_truthy
       expect(game.current_game_question.help_hash[:fifty_fifty]).to be
-      expect(game.current_game_question.help_hash[:fifty_fifty]).to  include('d') #d правильный ответ в тестовой фабрике
+      expect(game.current_game_question.help_hash[:fifty_fifty]).to include('d') #d правильный ответ в тестовой фабрике
       expect(game.current_game_question.help_hash[:fifty_fifty].size).to eq 2
       expect(response).to redirect_to(game_path(game))
     end
